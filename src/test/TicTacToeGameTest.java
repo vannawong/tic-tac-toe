7fc3892cfbcca1ab509023a6e2e27d387defa086
Huang <jhuang@webmdhealth.net>
2017-07-06 13:11:54 -0400

    added unit tests

--- src/test/TicTacToeGameTest.java
+++ src/test/TicTacToeGameTest.java
@@ -19,11 +19,83 @@ public class TicTacToeGameTest {
      * This test will check that, given a winning move, checkRows will return true.
      */
     @Test
-    public void givenPlayerOneWins_thenGameEndsAndPlayerOneScoreIncreased() {
+    public void checkColumnsTest1() {
+        int [][] board = new int[3][3];
+        board[0][0] = 1;
+        board[1][0] = 1;
+        board[2][0] = 1;
+        // board should now look like this:
+        // 1 0 0
+        // 1 0 0
+        // 1 0 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkColumn should return true", underTest.checkColumns(new Move(2, 0, 1)));
+    }
+
+    @Test
+    public void checkColumnsTest2() {
+        int [][] board = new int[3][3];
+        board[0][1] = 1;
+        board[1][1] = 1;
+        board[2][1] = 1;
+        // board should now look like this:
+        // 0 1 0
+        // 0 1 0
+        // 0 1 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkColumn should return true", underTest.checkColumns(new Move(2, 1, 1)));
+    }
+
+    @Test
+    public void checkColumnsTest5x5() {
+        int [][] board = new int[5][5];
+        board[0][0] = 1;
+        board[1][0] = 1;
+        board[2][0] = 1;
+        board[3][0] = 1;
+        board[4][0] = 1;
+        // board should now look like this:
+        // 1 0 0 0 0
+        // 1 0 0 0 0
+        // 1 0 0 0 0
+        // 1 0 0 0 0
+        // 1 0 0 0 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkRows should return true", underTest.checkColumns(new Move(4, 0, 1)));
+    }
+
+    @Test
+    public void checkRowsTest1() {
+        int [][] board = new int[3][3];
+        board[1][0] = 1;
+        board[1][1] = 1;
+        board[1][2] = 1;
+        // board should now look like this:
+        // 0 0 0
+        // 1 1 1
+        // 0 0 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkRows should return true", underTest.checkRows(new Move(1, 1, 1)));
+    }
+
+    @Test
+    public void checkRowsTest2() {
         int [][] board = new int[3][3];
         board[0][0] = 1;
         board[0][1] = 1;
-
+        board[0][2] = 1;
         // board should now look like this:
         // 1 1 1
         // 0 0 0
@@ -33,12 +105,109 @@ public class TicTacToeGameTest {
         underTest.setBoard(board);
 
         assertTrue("checkRows should return true", underTest.checkRows(new Move(0, 2, 1)));
-        assertEquals("player one score should increase", 1, underTest.getPlayerOneScore());
+    }
+
+    @Test
+    public void checkRowsTest5x5() {
+        int [][] board = new int[5][5];
+        board[0][0] = 1;
+        board[0][1] = 1;
+        board[0][2] = 1;
+        board[0][3] = 1;
+        board[0][4] = 1;
+        // board should now look like this:
+        // 1 1 1 1 1
+        // 0 0 0 0 0
+        // 0 0 0 0 0
+        // 0 0 0 0 0
+        // 0 0 0 0 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkRows should return true", underTest.checkRows(new Move(0, 4, 1)));
+    }
+
+    //This test looks for diagonal goes from top left to bottom right
+    @Test
+    public void checkTopLeftDiagonalTest() {
+        int [][] board = new int[3][3];
+        board[0][0] = 1;
+        board[1][1] = 1;
+        board[2][2] = 1;
+        // board should now look like this:
+        // 1 0 0
+        // 0 1 0
+        // 0 0 1
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkDiagonal should return true", underTest.checkDiagonals(new Move(2, 2, 1)));
+    }
+
+
+    //This test looks for diagonal from bottom left to top right
+    @Test
+
+    public void checkBottomLeftDiagonalTest() {
+        int [][] board = new int[3][3];
+        board[0][2] = 1;
+        board[1][1] = 1;
+        board[2][0] = 1;
+        // board should now look like this:
+        // 0 0 1
+        // 0 1 0
+        // 1 0 0
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkDiagonal should return true", underTest.checkDiagonals(new Move(2, 0, 1)));
+    }
+
+
+    @Test
+    public void checkTopLeftDiagonalTest5x5() {
+        int [][] board = new int[5][5];
+        board[0][0] = 1;
+        board[1][1] = 1;
+        board[2][2] = 1;
+        board[3][3] = 1;
+        board[4][4] = 1;
+        // board should now look like this:
+        // 1 0 0 0 0
+        // 0 1 0 0 0
+        // 0 0 1 0 0
+        // 0 0 0 1 0
+        // 0 0 0 0 1
+        // so 0 will denote an empty spot
+        // therefore, if we check row on this board, we should detect a winning condition
+        underTest.setBoard(board);
+
+        assertTrue("checkDiagonal should return true", underTest.checkDiagonals(new Move(4, 4, 1)));
     }
 
     @Test
     public void givenPlayerMove_andGameNotEnd_thenNeitherPlayersScoreIncreases() {
+        int [][] board = new int[3][3];
+        board[0][0] = 1;
+        board[1][0] = 1;
+        underTest.setBoard(board);
+        underTest.checkColumns(new Move(1,0,1));
+        assertEquals("player one score should stay", 0, underTest.getPlayerOneScore());
 
+    }
+
+    @Test
+    public void givenPlayerMove_andGameEnd_thenPlayersScoreIncreases() {
+        int [][] board = new int[3][3];
+        board[0][0] = 1;
+        board[1][0] = 1;
+        board[2][0] = 1;
+        underTest.setBoard(board);
+        underTest.checkColumns(new Move(2,0,1));
+        assertEquals("player one score should increase", 1, underTest.getPlayerOneScore());
     }
 
     // sets the given row of the given board to the player
